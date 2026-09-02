259ed34a577f9a1568e013371a216fc823cf154c
jayadeyemi <jayadeyemi@users.noreply.github.com>
2026-09-02 00:14:45 -0400

    Target ORC pre-prefix deployment name

--- scripts/tools/validate-controller-scale-defaults.sh
+++ scripts/tools/validate-controller-scale-defaults.sh
@@ -71,6 +71,8 @@ caaph_args=$(yq -r '.addon.helm.deployment.containers[] | select(.name == "manag
   ."kube-api-qps",."kube-api-burst"] | join(",")' <<<"${caaph_args}")   == "10,10,20,30" ]] || die "CAAPH defaults drifted"
 
 [[ $(yq -r '.spec.source.path' "${REPO_ROOT}/controllers/orc.yaml") == config/default ]]   || die "ORC must render the pinned Kustomize package"
+[[ $(yq -r '.spec.source.kustomize.patches[0].target.name' "${REPO_ROOT}/controllers/orc.yaml") == controller-manager ]] \
+  || die "ORC patch must target the upstream pre-prefix Deployment name"
 orc_patch=$(yq -r '.spec.source.kustomize.patches[0].patch' "${REPO_ROOT}/controllers/orc.yaml")
 [[ $(yq -r '[.spec.template.spec.containers[0].image,
   (.spec.template.spec.containers[0].args[] | select(. == "--scope-cache-max-size=10"))] | join(",")' \
